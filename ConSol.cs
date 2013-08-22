5cdf73d7dbbf543fe3d3b75eec2c8267b423cf4c
Olivier <olivierlizotte@gmail.com>
2013-08-22 15:07:56 -0400

    Added gtIgnore

---
+++ ConSol.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alchemy.Classes;
+
+namespace Afinity
+{
+    public class ConSol
+    {
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected ConcurrentDictionary<string, ConSolUser> OnlineUsersByName = new ConcurrentDictionary<string, ConSolUser>();
+
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected ConcurrentDictionary<UserContext, ConSolUser> OnlineUsersByContext = new ConcurrentDictionary<UserContext, ConSolUser>();
+
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected Logger Logs;
+        public ConSol()
+        {
+            Logs = new Logger();
+        }
+
+        public void WriteLine(string msg)
+        {
+            foreach(ConSolUser u in OnlineUsersByName.Values)//TODO for debugging purposes, send outputs everywhere
+                u.WriteLine(msg);
+            Console.WriteLine(msg);
+        }
+
+        public ConSolUser CreateUserConSol(UserContext context, string name)
+        {
+            if (OnlineUsersByName.ContainsKey(name))
+                return OnlineUsersByName[name];
+            else
+            {
+                ConSolUser user = new ConSolUser(context, name, Logs);
+                OnlineUsersByName.TryAdd(name, user);
+                OnlineUsersByContext.TryAdd(context, user);
+                return user;
+            }
+        }
+
+        public ConSolUser RemoveUserConSol(UserContext context)
+        {
+            try
+            {
+                if (OnlineUsersByContext.ContainsKey(context))
+                {
+                    ConSolUser user = OnlineUsersByContext[context];
+                    OnlineUsersByContext.TryRemove(context, out user);
+                    OnlineUsersByName.TryRemove(user.Name, out user);
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                WriteLine(ex.StackTrace);
+            }
+            return null;
+        }
+
+        public void UpdateLogFile()
+        {
+            Logs.UpdateLogFile();
+        }
+    }
+
+    public class ConSolUser
+    {
+        private Logger Logs;
+        public string Name = String.Empty;
+        public UserContext Context { get; set; }
+
+        public ConSolUser(UserContext userContext, string name, Logger log)
+        {
+            this.Context = userContext;
+            this.Name = name;
+            this.Logs = log;
+        }
+
+        public void WriteLine(string msg)
+        {
+            Logs.Add(msg, Name);
+            Context.Send(msg);
+        }
+    }
+}
---
+++ Logs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Afinity
+{
+    public class Logger
+    {
+        public static string FOLDER = "";
+        public static object synchroton = new object();
+
+        private List<string> NewLines = new List<string>(100);
+
+        public Logger()
+        {
+            System.Threading.TimerCallback tc = new System.Threading.TimerCallback(Tick);
+            System.Threading.Timer objTimer = new System.Threading.Timer(tc);
+            objTimer.Change(0, 150000);
+        }
+
+        //Update the log file
+        private void Tick(object data)
+        {
+            UpdateLogFile();
+        }
+
+        public void Add(string msg, string username)
+        {
+            lock (synchroton)
+            {
+                NewLines.Add(username + " : " + msg);
+            }
+        }
+
+
+        public void UpdateLogFile()
+        {
+            try
+            {
+                lock (synchroton)
+                {
+                    if (NewLines.Count > 0)
+                    {
+                        //lets create a log file with the name of the user. (needs write access to the executing folder)
+                        if (string.IsNullOrEmpty(FOLDER))
+                        {
+                            FOLDER = AppDomain.CurrentDomain.BaseDirectory + "Logs" + System.IO.Path.DirectorySeparatorChar;
+                            if (!System.IO.Directory.Exists(FOLDER))
+                                System.IO.Directory.CreateDirectory(FOLDER);
+                        }
+
+                        string fileName = System.DateTime.Today.Day + "_" + System.DateTime.Today.Month + "_" + System.DateTime.Today.Year + ".txt";
+                        System.IO.StreamWriter outFile = new System.IO.StreamWriter(FOLDER + System.IO.Path.DirectorySeparatorChar + fileName, true);
+
+                        foreach (string line in NewLines)
+                            outFile.WriteLine(line.ToString());
+
+                        outFile.Close();
+                        NewLines.Clear();
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                //Exceptionnally here, since its a log failing, do nothing!
+            }
+        }
+    }
+}
