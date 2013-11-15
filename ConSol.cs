d4234721158b98b30ea9ee831f3362d826851048
Olivier <olivierlizotte@gmail.com>
2013-11-15 15:55:05 -0500

    Switched to Fleck WebSocket Server

--- ConSol.cs
+++ ConSol.cs
@@ -4,10 +4,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Alchemy.Classes;
+using Fleck;
 
 namespace Afinity
 {
-    public class ConSol: Proteomics.Utilities.IConSol
+    public class ConSolAlchemy: Proteomics.Utilities.IConSol
+    {
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected ConcurrentDictionary<string, ConSolUserAlchemy> OnlineUsersByName = new ConcurrentDictionary<string, ConSolUserAlchemy>();
+
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected ConcurrentDictionary<UserContext, ConSolUserAlchemy> OnlineUsersByContext = new ConcurrentDictionary<UserContext, ConSolUserAlchemy>();
+
+        /// <summary>
+        /// Store the list of online users. Wish I had a ConcurrentList. 
+        /// </summary>
+        protected Logger Logs;
+        public ConSolAlchemy()
+        {
+            Logs = new Logger();
+        }
+
+        public void WriteLine(string msg)
+        {
+            foreach (ConSolUserAlchemy u in OnlineUsersByName.Values)//TODO for debugging purposes, send outputs everywhere
+                u.WriteLine(msg);
+            Console.WriteLine(msg);
+        }
+
+        public ConSolUserAlchemy GetConSolUser(UserContext context)
+        {
+            if (OnlineUsersByContext.ContainsKey(context))
+                return OnlineUsersByContext[context];
+            else
+                return null;
+        }
+
+        public ConSolUserAlchemy CreateUserConSol(UserContext context, string name, Func<string, UserContext, int> sendMessageFunc)
+        {
+            if (OnlineUsersByName.ContainsKey(name))
+                return OnlineUsersByName[name];
+            else
+            {
+                ConSolUserAlchemy user = new ConSolUserAlchemy(context, name, Logs, sendMessageFunc);
+                OnlineUsersByName.TryAdd(name, user);
+                OnlineUsersByContext.TryAdd(context, user);
+                return user;
+            }
+        }
+
+        public ConSolUserAlchemy RemoveUserConSol(UserContext context)
+        {
+            try
+            {
+                if (OnlineUsersByContext.ContainsKey(context))
+                {
+                    ConSolUserAlchemy user = OnlineUsersByContext[context];
+                    OnlineUsersByContext.TryRemove(context, out user);
+                    OnlineUsersByName.TryRemove(user.Name, out user);
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                WriteLine(ex.StackTrace);
+            }
+            return null;
+        }
+
+        public void UpdateLogFile()
+        {
+            Logs.UpdateLogFile();
+        }
+    }
+
+    public class ConSolUserAlchemy: Proteomics.Utilities.IConSol
+    {
+        private Logger Logs;
+        public string Name = String.Empty;
+        public UserContext Context { get; set; }
+        private Func<string, UserContext, int> SendMessageFunc;
+
+        public ConSolUserAlchemy(UserContext userContext, string name, Logger log, Func<string, UserContext, int> sendMessage)
+        {
+            this.Context = userContext;
+            this.Name = name;
+            this.Logs = log;
+            this.SendMessageFunc = sendMessage;
+        }
+
+        public void WriteLine(string msg)
+        {
+            Logs.Add(msg, Name);
+            SendMessageFunc(msg, Context);
+            //Context.Send(msg);
+            
+        }
+    }
+
+    public class ConSol : Proteomics.Utilities.IConSol
     {
         /// <summary>
         /// Store the list of online users. Wish I had a ConcurrentList. 
@@ -17,7 +117,7 @@ namespace Afinity
         /// <summary>
         /// Store the list of online users. Wish I had a ConcurrentList. 
         /// </summary>
-        protected ConcurrentDictionary<UserContext, ConSolUser> OnlineUsersByContext = new ConcurrentDictionary<UserContext, ConSolUser>();
+        protected ConcurrentDictionary<IWebSocketConnection, ConSolUser> OnlineUsersByContext = new ConcurrentDictionary<IWebSocketConnection, ConSolUser>();
 
         /// <summary>
         /// Store the list of online users. Wish I had a ConcurrentList. 
@@ -30,17 +130,20 @@ namespace Afinity
 
         public void WriteLine(string msg)
         {
-            foreach(ConSolUser u in OnlineUsersByName.Values)//TODO for debugging purposes, send outputs everywhere
+            foreach (ConSolUser u in OnlineUsersByName.Values)//TODO for debugging purposes, send outputs everywhere
                 u.WriteLine(msg);
             Console.WriteLine(msg);
         }
 
-        public ConSolUser GetConSolUser(UserContext context)
+        public ConSolUser GetConSolUser(IWebSocketConnection context)
         {
-            return OnlineUsersByContext[context];
+            if (OnlineUsersByContext.ContainsKey(context))
+                return OnlineUsersByContext[context];
+            else
+                return null;
         }
 
-        public ConSolUser CreateUserConSol(UserContext context, string name, Func<string, UserContext, int> sendMessageFunc)
+        public ConSolUser CreateUserConSol(IWebSocketConnection context, string name, Func<string, IWebSocketConnection, int> sendMessageFunc)
         {
             if (OnlineUsersByName.ContainsKey(name))
                 return OnlineUsersByName[name];
@@ -53,7 +156,13 @@ namespace Afinity
             }
         }
 
-        public ConSolUser RemoveUserConSol(UserContext context)
+        public IEnumerable<ConSolUser> GetAllConnectedClients()
+        {
+            foreach (ConSolUser user in OnlineUsersByName.Values)
+                yield return user;
+        }
+
+        public ConSolUser RemoveUserConSol(IWebSocketConnection context)
         {
             try
             {
@@ -79,14 +188,14 @@ namespace Afinity
         }
     }
 
-    public class ConSolUser: Proteomics.Utilities.IConSol
+    public class ConSolUser : Proteomics.Utilities.IConSol
     {
         private Logger Logs;
         public string Name = String.Empty;
-        public UserContext Context { get; set; }
-        private Func<string, UserContext, int> SendMessageFunc;
+        public IWebSocketConnection Context { get; set; }
+        private Func<string, IWebSocketConnection, int> SendMessageFunc;
 
-        public ConSolUser(UserContext userContext, string name, Logger log, Func<string, UserContext, int> sendMessage)
+        public ConSolUser(IWebSocketConnection userContext, string name, Logger log, Func<string, IWebSocketConnection, int> sendMessage)
         {
             this.Context = userContext;
             this.Name = name;
@@ -98,8 +207,6 @@ namespace Afinity
         {
             Logs.Add(msg, Name);
             SendMessageFunc(msg, Context);
-            //Context.Send(msg);
-            
         }
     }
 }
